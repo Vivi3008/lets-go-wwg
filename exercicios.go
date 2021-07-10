feed7edf49cbf8145d4b5fa7075790093bed5e57
viviRamos <financeiro@portalautopecas.com.br>
2021-07-10 17:36:50 -0300

    chore

--- exercicios.go
+++ exercicios.go
@@ -55,4 +55,40 @@ func main() {
 
 	exemplo2 := make([]string, 5, 7)
 	fmt.Println("tamanho: ", len(exemplo2))
+	exemplo2[3] = "Wednesday"
+	fmt.Println("tamanho do slice: ", len(exemplo2))
+	fmt.Println("capacidade do slice: ", cap(exemplo2))
+
+	fmt.Println(exemplo2[:])                //imprime todos valores do slice
+	fmt.Println(exemplo2[0:1])              //imprime o valor da posicao 1
+	fmt.Println(exemplo2[len(exemplo2)-1:]) //imprime o ultimo elemento do slice
+
+	//crie um slice do tamanho 12 usando make() e atribua os signos do zodiaco a cada um dos elementos individualmente
+	//printe na tela essa slice
+	signos := make([]string, 12, 12)
+
+	signos = append(signos, "Aquario", "Peixes", "Áries", "Touro", "Gemẽos", "Câncer", "Leão", "Virgem", "Libra", "Escorpião", "Sagitário", "Capricórnio")
+	fmt.Println("Signos do zodiaco: ", signos[:])
+	fmt.Println(len(signos))
+
+	//crie um mapa onde as chaves(key) sao os numeros dos meses e o valor é o nome do mes
+	//printe os meses janeiro e dezembro
+
+	var meses = make(map[int16]string)
+	meses[1] = "janeiro"
+	meses[2] = "fevereiro"
+	meses[3] = "março"
+	meses[4] = "abril"
+	meses[5] = "maio"
+	meses[6] = "junho"
+	meses[7] = "JULHO"
+	meses[8] = "agosto"
+	meses[9] = "setembro"
+	meses[10] = "outubro"
+	meses[11] = "novembro"
+	meses[12] = "dezembro"
+
+	fmt.Println(meses)
+	fmt.Println(meses[1])
+	fmt.Println(meses[12])
 }
