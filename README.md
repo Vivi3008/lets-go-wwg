c0b3746901a5d54ff830a4f52a33997cfe08ec81
viviRamos <financeiro@portalautopecas.com.br>
2021-07-10 15:35:07 -0300

    chore

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # lets-go-wwg
 
 Curso de Golang feito por [Women Who Go Curitiba](https://womenwhogocwb.github.io/)
+[Conteúdo](https://womenwhogocwb.gitbook.io/letsgo/)
--- exercicios.go
+++ exercicios.go
@@ -21,4 +21,38 @@ func main() {
 	fmt.Printf("O tipo de b é %T e seu valor é %v\n", b, b)
 	fmt.Printf("O tipo de soma é %T e seu valor é %v\n", soma, soma)
 	fmt.Println(soma)
+
+	nome2 := "viviane"
+	cor := "roxo"
+
+	fmt.Printf("Meu nome é %s e minha cor preferida é %s!\n", nome2, cor)
+
+	anoNascimento := 1986
+	anoAtual := 2021
+
+	age := (anoAtual - anoNascimento)
+
+	fmt.Printf("Minha idade é %v\n", age)
+
+	//arrays
+
+	var weekdays [7]string
+	weekdays = [7]string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+	fmt.Println(weekdays[2])
+
+	//slices
+	var diasDaSemana []string
+	fmt.Println("tamanho do slice: ", len(diasDaSemana))
+
+	diasDaSemana = append(diasDaSemana, "Sunday", "Monday")
+
+	//depois do append
+	fmt.Println("tamanho do slice:", len(diasDaSemana))
+
+	//make()
+	exemplo := make([]string, 7)
+	fmt.Println("tamanho: ", len(exemplo))
+
+	exemplo2 := make([]string, 5, 7)
+	fmt.Println("tamanho: ", len(exemplo2))
 }
